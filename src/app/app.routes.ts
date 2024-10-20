bedc9290eda4fbd76f0c6019507ba3332d733283
TMedalith <u20211f955@upc.edu.pe>
2024-10-20 15:21:16 -0500

    refactor: Update app.config.ts and app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { authenticationGuard } from './iam/services/authentication.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout.component'),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./dashboard/pages/dashboard/dashboard.component'),
+                canActivate: [authenticationGuard]
+            }, 
+            {
+                path: 'devices',
+                loadComponent: () => import('./devices/pages/devices/devices.component'),
+                canActivate: [authenticationGuard]
+
+          
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: 'sign-in',
+        loadComponent: () => import('./iam/pages/sign-in/sign-in.component')
+    },
+    {
+        path: 'sign-up',
+        loadComponent: () => import('./iam/pages/sign-up/sign-up.component')
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    }
+];
