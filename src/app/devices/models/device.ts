1d8493087aabddbaddfbf29230c4df78bcd397b6
TMedalith <u20211f955@upc.edu.pe>
2024-10-27 14:01:58 -0500

    refactor: Add properties and constructor to Device model

--- src/app/devices/models/device.ts
+++ src/app/devices/models/device.ts
@@ -1,1 +1,18 @@
-export class Device {}
+export class Device {
+  id: number;
+  name: string;
+  status: 'active' | 'inactive';
+  lastReading: Date;
+  type: string;
+  cropId: number;
+  installationDate: Date;
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.status = 'inactive';
+    this.lastReading = new Date();
+    this.type = '';
+    this.cropId = 0;
+    this.installationDate = new Date();
+  }
+}
