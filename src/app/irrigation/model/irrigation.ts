021d74cb67d11f3e25ec0873f0a3014a12b5f883
TMedalith <u20211f955@upc.edu.pe>
2024-10-29 15:56:53 -0500

    refactor: Update Irrigation model

--- src/app/irrigation/model/irrigation.ts
+++ src/app/irrigation/model/irrigation.ts
@@ -1,1 +1,27 @@
-export class Irrigation {}
+export class Irrigation {
+  id: number;
+  cropId: number;
+  mode: 'manual' | 'automatic';
+  automaticIrrigationEnabled: boolean;
+  manualIrrigationEnabled: boolean;
+  sensorThreshold: number;
+  waterAmount: number;
+  currentSoilMoisture: number;
+  consentGiven: boolean;
+  isCurrentlyIrrigating: boolean;
+  duration: number;
+
+  constructor() {
+    this.id = 0;
+    this.cropId = 0;
+    this.mode = 'manual';
+    this.automaticIrrigationEnabled = false;
+    this.manualIrrigationEnabled = false;
+    this.sensorThreshold = 30;
+    this.waterAmount = 0;
+    this.currentSoilMoisture = 25;
+    this.consentGiven = false;
+    this.isCurrentlyIrrigating = false;
+    this.duration = 0;
+  }
+}
