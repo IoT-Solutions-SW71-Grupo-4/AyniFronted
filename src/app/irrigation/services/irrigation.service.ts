802a4307b93dd539d15a1abf345a9a791fd61d55
TMedalith <u20211f955@upc.edu.pe>
2024-10-29 15:57:13 -0500

    refactor: Update IrrigationService to extend BaseService

--- src/app/irrigation/services/irrigation.service.ts
+++ src/app/irrigation/services/irrigation.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Irrigation } from '../model/irrigation';
+import { BaseService } from '../../shared/services/base.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class IrrigationService {
-  constructor() {}
+export class IrrigationService extends BaseService<Irrigation> {
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = '/irrigation';
+  }
 }
