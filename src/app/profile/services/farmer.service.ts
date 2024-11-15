ce59e7508f78f950e24e6fcbff9915dd9682efc2
TMedalith <u20211f955@upc.edu.pe>
2024-11-15 13:59:58 -0500

    feat: Update updateFarmerImage method to accept FormData and include authorization header

--- src/app/profile/services/farmer.service.ts
+++ src/app/profile/services/farmer.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Farmer } from '../model/farmer';
 import { BaseService } from '../../shared/services/base.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -14,11 +14,15 @@ export class FarmerService extends BaseService<Farmer> {
     this.resourceEndpoint = '/farmers';
   }
 
-  updateFarmerImage(farmerId: number, imageFile: File): Observable<Farmer> {
-    const formData = new FormData();
-    formData.append('file', imageFile);
-    return this.http.put<Farmer>(`${this.resourcePath()}/${farmerId}/farmerImage`, formData);
+  updateFarmerImage(farmerId: number, formData: FormData): Observable<Farmer> {
+   
+    return this.http.put<Farmer>(`${this.resourcePath()}/${farmerId}/farmerImage`, formData, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      }),
+    });
   }
+  
 
   deleteFarmerImage(farmerId: number): Observable<void> {
     return this.http.delete<void>(`${this.resourcePath()}/${farmerId}/farmerImage`, this.httpOptions);
