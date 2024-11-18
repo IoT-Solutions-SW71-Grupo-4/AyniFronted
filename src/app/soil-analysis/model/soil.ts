051b59f5cbf26882407302eb9edd7b62cbaf0753
TMedalith <u20211f955@upc.edu.pe>
2024-11-18 16:23:31 -0500

    feat: Add Weather model and service for weather report management; refactor device and soil analysis models; update styles and remove unused components

--- src/app/soil-analysis/model/soil.ts
+++ src/app/soil-analysis/model/soil.ts
@@ -1,23 +1,20 @@
 export class Analysis {
   id: number;
-  cropId: number;
+  date: string;
   deviceId: number;
-  analysisDate: string;
-  pHLevel: number;
-  temperature: number;
-  moisture: number;
+
+  
+
   nitrogen: number;
   phosphorus: number;
   potassium: number;
+temperature: any;
+humidity: any;
 
   constructor() {
     this.id = 0;
-    this.cropId = 0;
     this.deviceId = 0;
-    this.analysisDate = '';
-    this.pHLevel = 0;
-    this.temperature = 0;
-    this.moisture = 0;
+    this.date = '';
     this.nitrogen = 0;
     this.phosphorus = 0;
     this.potassium = 0;
---
+++ src/app/soil-analysis/model/weather.ts
@@ -0,0 +1,18 @@
+export class Weather {
+    id: number;
+    date: string;
+    temperature: number;
+    humidity: number;
+    deviceId: number;
+  
+    
+  
+    constructor() {
+      this.id = 0;
+      this.deviceId = 0;
+      this.date = '';
+      this.temperature = 0;
+        this.humidity = 0;
+    }
+  }
+  
